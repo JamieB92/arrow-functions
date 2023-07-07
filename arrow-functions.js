9baecefd5e97e864ff2001a0b30d979d7c670741
JamieB92 <j.bonner6992@gmail.com>
2023-07-07 20:57:23 +0000

    Add Lesson

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,33 @@
  */
 
 // Vanilla JavaScript Function
+// function addTwoNumbers(a, b){
+//     return a + b
+// }
+// let sum = addTwoNumbers(3, 5);
 
+// console.log(sum)
+    
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) =>{
+    return a + b
+}
+let sum = addTwoNumbers(3, 5);
+
+console.log(sum)
 
 // Single Line Arrow Function With Parameters
-
+const addTwoNumbers2 = (a, b) => a + b;
 // Implicit Returns
 
+const saySomething = message => console.log(message)
+saySomething('Hi')
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+    Inside the multi string
+    </p>`
+)
+console.log(returnMultipleLines())
